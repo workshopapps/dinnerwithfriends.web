9fc9da978bb889c089bf3a970ee2a27f1950dadc
Donice <doniceubaru1@gmail.com>
2022-11-17 12:59:53 +0100

    Set up the repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Project 22</h1>
     </div>
   );
 }
