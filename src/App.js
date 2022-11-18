e63ca2284b9944383c2fa0e2ade929ddcf02c9c3
Pappyjay23 <pjinadu02@gmail.com>
2022-11-18 05:52:07 +0100

    Updated calendar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import SingleCalendar from "./components/SingleCalendar/SingleCalendar";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Project 22</h1>
+      <div className='w-[40%] mx-auto'>
+        <SingleCalendar  range={false}/>
+      </div>
     </div>
   );
 }
--- src/components/SingleCalendar/SingleCalendar.jsx
+++ src/components/SingleCalendar/SingleCalendar.jsx
@@ -1,15 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
-import './SingleCalendar.css'
+import TimePicker from "react-time-picker";
+import "./SingleCalendar.css";
 
-const SingleCalendar = () => {
-  return (
-    <div className='text-sm'>
-      <Calendar 
+const SingleCalendar = ({ range }) => {
+	const [value, setValue] = useState(new Date());
+	const [time, setTime] = useState("17:00");
+	const [dates, setDates] = useState([]);
+	const [times, setTimes] = useState([]);
+	const [dateLimit, setDateLimit] = useState(false);
+
+	const valueRange =
+		value.length > 1 &&
+		value.map((item) => {
+			return item.toDateString();
+		});
+	const valueRangeString = `Date range from ${valueRange[0]} to ${valueRange[1]}`;
+
+	const setDT = (valueRangeString) => {
+		setDates([...dates, !range ? value.toDateString() : valueRangeString]);
+		setTimes([...times, time]);
+	};
+
+	useEffect(() => {
+		if (dates.length > 10) {
+			setDateLimit(true);
+		}
+	}, [dates]);
+
+	return (
+		<div className='text-xs rounded-[8px] border border-[#66a3ff] p-1'>
+			<div className='mt-2 text-center flex justify-center'>
+				<div
+					className={`bg-red-600 w-fit py-2 px-6 text-white transition-all duration-150 ${
+						dateLimit ? "opacity-100" : "opacity-0"
+					}`}>
+					No more dates available.
+				</div>
+			</div>
+			<Calendar
 				calendarType='US'
-				selectRange />
-    </div>
-  )
-}
+				selectRange={range ? true : false}
+				onChange={(e) => {
+					setValue(e);
+				}}
+				value={value}
+			/>
+			<div className='px-10 pb-8'>
+				<span className='mr-4'>Time:</span>
+				<TimePicker
+					onChange={(e) => {
+						setTime(e);
+					}}
+					value={time}
+					disableClock
+					clearIcon={null}
+				/>
+				<div className='mt-6'>
+					<button
+						disabled={dateLimit ? true : false}
+						onClick={() => setDT(valueRangeString)}
+						className={`rounded-[8px] ${
+							dateLimit ? "bg-blue-300" : "bg-blue-600"
+						} p-2 text-white`}>
+						Add date and time
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default SingleCalendar
+export default SingleCalendar;
