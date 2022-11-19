92d5ab4dc5a610ea02ff6aeaccb13afdf5fb33ae
Ejim Favour <favourejim56@gmail.com>
2022-11-19 13:05:56 +0100

    refactor: links under company refactored

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {BsFillSuitHeartFill} from "react-icons/bs"
+import { BsFillSuitHeartFill } from "react-icons/bs";
 
 const Footer = () => {
-  const company = ['Careers','About us', 'Contact us', 'Partner with us']
-  const learn = ['Blogs', 'Webinars', 'How it works']
-  const legal = ['Security', 'Privacy policy', 'Cookies Setting', 'Terms & Conditions']
-  const help = ['FAQs', 'Help Center', 'Customer support']
+  const company = [
+    { title: "Careers", to: "careers" },
+    { title: "About us", to: "about-us" },
+    { title: "Contact us", to: "contact-us" },
+    { title: "Partner with us", to: "partner-with-us" },
+  ];
+  const learn = ["Blogs", "Webinars", "How it works"];
+  const legal = [
+    "Security",
+    "Privacy policy",
+    "Cookies Setting",
+    "Terms & Conditions",
+  ];
+  const help = ["FAQs", "Help Center", "Customer support"];
 
   return (
     <footer class="bg-blue-700">
-
       <div class="mx-auto lg:mx-24 grid grid-cols-2 gap-8 py-8 px-6 md:grid-cols-5">
         <div>
-        <Link to="/" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <span className="self-center text-white text-3xl font-semibold whitespace-nowrap ">
-            Catch
+              Catch
             </span>
             <span className="self-center text-black text-3xl font-semibold whitespace-nowrap">
-            Up
+              Up
             </span>
           </Link>
         </div>
@@ -27,12 +36,12 @@ const Footer = () => {
             Company
           </h2>
           <ul class="text-gray-100">
-            {company.map((e) => (
-              <li class="mb-1 lg:mb-4">
-              <Link href="#" class=" hover:underline">
-                {e}
-              </Link>
-            </li>
+            {company.map((item, i) => (
+              <li class="mb-1 lg:mb-4" key={i}>
+                <Link to={item.to} class=" hover:underline">
+                  {item.title}
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
@@ -41,12 +50,12 @@ const Footer = () => {
             Help center
           </h2>
           <ul class="text-gray-100">
-          {learn.map((e) => (
+            {learn.map((e) => (
               <li class="mb-1 lg:mb-4">
-              <Link href="#" class=" hover:underline">
-                {e}
-              </Link>
-            </li>
+                <Link href="#" class=" hover:underline">
+                  {e}
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
@@ -55,12 +64,12 @@ const Footer = () => {
             Legal
           </h2>
           <ul class="text-gray-100">
-          {legal.map((e) => (
+            {legal.map((e) => (
               <li class="mb-1 lg:mb-4">
-              <Link href="#" class=" hover:underline">
-                {e}
-              </Link>
-            </li>
+                <Link href="#" class=" hover:underline">
+                  {e}
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
@@ -69,20 +78,20 @@ const Footer = () => {
             Download
           </h2>
           <ul class="text-gray-100">
-          {help.map((e) => (
+            {help.map((e) => (
               <li class="mb-1 lg:mb-4">
-              <Link href="#" class=" hover:underline">
-                {e}
-              </Link>
-            </li>
+                <Link href="#" class=" hover:underline">
+                  {e}
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
       </div>
       <div class="mx-auto lg:mx-24 py-6 px-4 md:flex md:items-center md:justify-between">
         <span class="text-md text-gray-100 sm:text-center">
-          © 2022 <Link href="https://flowbite.com/">TEAM PRYBAR™</Link>. All Rights
-          Reserved.
+          © 2022 <Link href="https://flowbite.com/">TEAM PRYBAR™</Link>. All
+          Rights Reserved.
         </span>
         <div class="flex mt-1 text-md text-white space-x-6 sm:justify-center md:mt-0">
           Made with love <BsFillSuitHeartFill />
