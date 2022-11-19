3ce7d0ad403e5811dcdd6448c5b4c7236e9f8413
dwhyq <dwhyq@outlook.com>
2022-11-19 23:38:07 +0100

    fro-2-account-settings: (fix: routed the settings first page)

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/create_event" element={<CreateEvent />} />
         <Route path="/error" element={<Error />} />
         <Route path="/invitee" element={<Invitee />} />
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/" element={<ProfilePage />} />
+        <Route path="/ProfilePage" element={<ProfilePage />} />
         <Route path="/view_event" element={<ViewEvent />} />
         <Route path="/terms_of_use" element={<TermsOfUse />} />
         <Route path="/dashboard/event_summary" element={<EventSummary />} />
