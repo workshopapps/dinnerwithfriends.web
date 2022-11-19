f1fe3493d660e830e005e4616ba8a4eeb60df8c1
vickymarz <okorojiebube2@gmail.com>
2022-11-19 02:04:08 +0100

    fro-5: Add reusable Event component

---
+++ src/components/Event.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import calender from '../assets/img/calendar.png'
+
+const Event = ({ status }) => {
+  return (
+    <div className='flex justify-center irems-center g-y-4'>
+        <div className='w-[154px] h-[154px] lg:w-[199px] lg:h-[199px]'>
+          <img src={calender} alt="calender" />
+        </div>
+        <h4 className='text-[#1A1616] lg:font-bold text-xl'>No {status} events yet</h4>
+    </div>
+  )
+}
+
+Event.propTypes = {
+    status: PropTypes.string.isRequired,
+    className: PropTypes.string,
+};
+
+Event.defaultProps = {
+    className: '',
+}
+
+export default Event
--- src/pages/dashboard/UpcomingEvent.jsx
+++ src/pages/dashboard/UpcomingEvent.jsx
@@ -1,68 +1,71 @@
-import React from 'react'
+import React, { useState} from 'react'
+import Button from '../../components/Button'
 import Navbar from '../../components/Navbar'
+import calender from '../../assets/img/g-calendar.png'
+import MediaQuery from 'react-responsive'
+import Event from '../../components/Event'
 
 const UpcomingEvent = () => {
+  const [status, setStatus] = useState('Upcoming');
+
     return (
         <>
         <Navbar />
         <section className='px-[22px] lg:px-20 pt-11 lg:pt-16'>
-          <div className='flex flex-col justify-center lg:justify-between items:center gap-4'>
+          <div className='flex flex-col justify-center lg:justify-between items:center gap-4 mb-4 lg:mb-8'>
             <div className='flex flex-col justify-center lg:justify-start items:center lg:items-start gap-y-4'>
                <h2 className='text-[#151517] text-2xl font-bold lg:text-[40px]'>Welcome</h2>
                <p className='text-[#898989] font-bold lg:font-normal text-sm lg:text-lg'>Create events, send invites and keep up with your friends</p>
             </div>
+            <Button type='button' className='hidden lg:flex justify-between items-center gap-x-3.5 rounded-lg border border-solid border-[#344054] py-3.5 px-4 outline-0'>
+                <img src={calender} alt="google-calender" />
+                <p className='text-[#344054] text-lg'>Sync with Google Calendar</p>
+            </Button>
           </div>
-          <div className='mt-[44px] md:mt-[80px] text-center'>
-            <p className='flex flex-col md:flex-row justify-center items-center'>
-              <span className='text-[#061A37] font-bold text-[24px] md:text-[34px]'>Please select your availability</span>
-              <span className='text-[#898989] font-bold text-[14px] md:text-xl'>The highlighted date are the proposed date</span>
-            </p>
-            <p className='text-[#898989] font-bold text-[18px] md:text-[24px] my-[12px]'>By clicking on a date, you are indicating that you are available</p>
-          </div>
-          <div className='flex flex-col md:flex-row justify-between gap-[40px] md:gap-[80px] mb-10 md:mb-20 mt-10 md:mt-20'>
-            <div className='text-center flex flex-col justify-center items-center flex-1'>
-              <p className='font-bold text-xl text-[#717172]'>Select your dates here</p>
-              <DatePicker onChange={handleDatas} value={value} activeStartDate={availabilityStartDate}  minDate={availabilityStartDate} maxDate={availabilityEndDate}   defaultValue={preferredDate}/>
-            </div>
-            <div>
-              <h4 className='text-center font-bold text-xl text-[#717172]'>Selected dates will appear here</h4>
-              <div className='mt-[48px] flex flex-col gap-[20px] md:gap-[40px]'>
-                <div className='cursor-pointer flex gap-6 justify-start items-center'>
-                  <input
-                    type='checkbox'
-                    checked
-                    readOnly
-                    className={'w-5 h-5 outline-none bg-[#3B89FF] rounded-md'}
-                  />
-                  <span className='font-bold text-[#151517] md:text-2xl'>{myDate(availabilityStartDate)} by 12:30pm at VGC Abuja</span>
-                </div>
-                <div className='cursor-pointer flex gap-6 justify-start items-center'>
-                  <input
-                    type='checkbox'
-                    checked
-                    readOnly
-                    className={'w-5 h-5 outline-none bg-[#3B89FF] text-white rounded-md'}
-                  />
-                  <span className='font-bold text-[#151517] md:text-2xl'>{myDate(availabilityEndDate)} by 10:30am at VGC Abuja</span>
-                </div>
-              </div>
+          <div className='flex flex-col p-8 gap-y-8 justify-center items-center border border-solid border-[#CDCDCD] bg-[#FAFAFA] lg:bg-white'>
+            <Button type='button' className='font-bold text-sm lg:text-xl flex justify-between items-center gap-x-3.5 text-white bg-[#1070FF] rounded py-3 px-4 lg:px-8 outline-0'>
+              <span>+</span>
+              <span>Create Event</span>
+            </Button>
+            <div className='flex flex-col justify-center items-center gap-y-2'>
+              <span className='text-[#151517] font-bold lg:font-medium text-xl'>New Catch Up Event</span>
+              <span className='text-[#898989] font-bold lg:font-normal text-lg'>Create event and let people chip in</span>
             </div>
           </div>
-          <p className='text-[#717172] font-bold  md:text-[24px]'>By clicking submit, you confirm that you are available on the selected dates.</p>
-          <p className='text-[#424245] font-bold  md:text-[24px] mt-[22px]'>Don’t worry much, if your schedule is tight during the proposed date. You can request to reschedule by clicking the reschedule button below below</p>
-          <div className='flex justify-center items-center mt-[40px] mb-[59px] gap-x-[40px]'>
-            <Button type='button' className='bg-[#1070FF] text-white text-center rounded py-3 px-8' onClick={handleSubmit}>
-                Submit
-            </Button>
-            <Button type='button' className='bg-[#fff] text-white rounded border-solid border-2 border-[#1070FF] py-3 px-8 text-[#1070FF] bg-white text-center'>
-                Reschedule
-            </Button>
+          <div className='lg:border border-solid border-[#CDCDCD] px-[46px] py-9'>
+             <h3 className='text-[#151517] text-center lg:text-left font-bold lg:font-medium text-2xl mb-[40px]'>All Events</h3>
+             <MediaQuery minWidth={1024}>
+               <ul className='flex justify-start items-center gap-x-8'>
+                <li>
+                  <Button className='outline-0 border-0 text-[#717172] bg-inherit text-lg' onClick={() => setStatus('Upcoming')}>
+                    Upcoming Event
+                  </Button>
+                </li>
+                <li>
+                  <Button className='outline-0 border-0 text-[#717172] bg-inherit text-lg' onClick={() => setStatus('Rsvp')}>
+                   Rsvp Event
+                  </Button>
+                </li>
+                <li>
+                  <Button className='outline-0 border-0 text-[#717172] bg-inherit text-lg' onClick={() => setStatus('Pending')}>
+                    Pending Event
+                  </Button>
+                </li>
+               </ul>
+              </MediaQuery>
+          </div>
+          <div className='flex justify-center items-center mt-[40px] mb-[59px] lg:border border-solid border-[#CDCDCD]'>
+          {status === 'Upcoming' && (
+              <Event status={status}/>
+          )}
+          {status === 'Rsvp' && (
+              <Event status={status}/>
+          )}
+          {status === 'Pending' && (
+              <Event status={status}/>
+          )}
           </div>
         </section>
-        <DashboardFooter />
-        {modal && (
-              <EventResponse modal={modal} handleModal={handleModal} setModal={setModal}/>
-            )}
         </>
       )
 }
