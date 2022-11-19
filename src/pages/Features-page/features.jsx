c1bfc49775b1f1e2d1438ea30d03b207584e9f52
Halima360 <halimatuiyeibrahim360@gmail.com>
2022-11-19 22:25:54 +0100

    fro-17-features-page second commit

--- src/pages/Features-page/features.jsx
+++ src/pages/Features-page/features.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import F1 from "../../pages/Features-page/F1.png";
-import F2 from "../../pages/Features-page/F1.png";
+import F2 from "../../pages/Features-page/F2.png";
 import F3 from "../../pages/Features-page/F3.png";
 import Arrow from "../../pages/Features-page/arrow.png"
 export function Features(){
@@ -15,7 +15,7 @@ export function Features(){
                 <div>
                     <img className="w-80" src={F1}/>
                     </div>
-            <div className="flex flex-col items-start justify-center md:w-96 w-60  ">
+            <div className="flex flex-col-reverse items-start justify-center md:w-96 w-60  ">
                 <h1 className="font-bold">Availability Preferences</h1>
                 <p className="">One of our most important feautures at catch up, 
                     is the availability prefrences where your friends can notify you the host of
@@ -27,7 +27,7 @@ flex flex-row items-center justify-between space-x-5 "><p>Get Started</p>
             </div>
             
             </section>
-<section className="flex flex-col space-y-20 md:flex-row items-center justify-between px-20 bg-slate-100 p-20">
+<section className="flex flex-col-reverse space-y-40 md:flex-row items-center justify-between px-20 bg-slate-100 p-20">
     <div className="flex flex-col items-start justify-center md:w-96 w-60 ">
         <h1 className="font-bold">Sync Calendar</h1>
         <p>We have made it easier for our users to sync there 
@@ -35,10 +35,10 @@ google calendar to our platform through the channel we have created ,
  users can set reminders on there google calendar and not miss out on events from there friends.</p>
  <Link to="/sign_up" className="bg-blue-600 mt-5 text-white px-5 py-2  rounded  
 flex flex-row items-center justify-between space-x-5 "><p>Get Started</p> 
-<img  className="w-3" src={Arrow}/> </Link>
+<img  className="w-3 " src={Arrow}/> </Link>
     </div>
     <div>
-        <img className="w-80" src={F2}/>
+        <img className="w-80 mb-20 wd:mb-0" src={F2}/>
     </div>
 </section>
 <section className="flex flex-col space-y-20 md:flex-row items-center justify-between p-20">
