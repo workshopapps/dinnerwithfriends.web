c54e796d7fa16511b565bf9a9b3cd5418e1d0660
abel-otegbola <abel15655@gmail.com>
2022-11-20 08:38:10 +0100

    feat(FE-19): Finish up the terms of use page

--- src/pages/termsOfUse/index.js
+++ src/pages/termsOfUse/index.js
@@ -10,17 +10,22 @@ const TermsOfUse = () => {
   }
 
   return (
-    <div className='flex flex-col justify-end mx-4 mt-22 md:flex-row md:mx-10 '>
-        <div className='md:w-2/6 h-1/1 sm:w-auto md:fixed top-22 left-4 sticky m-2 mt-10'>
-            <h3 className='text-xl font-bold mb-5'>Content</h3>
+    <div className='flex justify-end flex-wrap flex-col mx-4 md:flex-row md:mx-10 m-5'>
+
+        <div className='md:w-4/6 w-full m-2 md:px-4'>
+          <p className='mb-5 font-bold text-3xl'>Terms of Use</p>
+          <p className='mt-2'>This document was last updated 15th of December, 2022. To start using our product, read this Terms and Conditions thoroughly.</p>
+        </div>
+
+        <div className='md:w-2/6 sm:w-auto md:sticky h-screen top-0 static px-2 md:-mt-24 mt-10 my-10'>
+            <h3 className='text-xl font-bold'>Content</h3>
             <ul>
               {
               termsData.map(term => {
                 return (
-                  <li className='my-5'>
-                    <a 
-                      key={term.id} 
-                      className={`my-7 font-medium text-base hover:text-blue-800 ${(active === term.heading) ? "text-blue-700" : ""}`} 
+                  <li className='my-5' key={term.id}>
+                    <a  
+                      className={`my-4 font-medium text-base hover:text-blue-800 ${(active === term.heading) ? "text-blue-700" : ""}`} 
                       href={`#${term.heading}`}
                       onClick={() => handleActive(term.heading)}
                     >{term.heading}</a>
@@ -31,9 +36,7 @@ const TermsOfUse = () => {
             </ul>
         </div>
 
-        <div className='md:w-4/6 w-auto m-2'>
-            <p className='my-5 font-bold text-3xl'>Terms of Use</p>
-            <p className='mt-2 mb-5'>This document was last updated 15th of December, 2022. To start using our product, read this Terms and Conditions thoroughly.</p>
+        <div className='md:w-4/6 w-auto p-2'>
 
             {
               termsData.map(term => {
@@ -41,10 +44,21 @@ const TermsOfUse = () => {
                   <div key={term.id} id={term.heading}>
                     <h3 className='my-3 mt-10 font-semibold text-base'>{term.heading}</h3>
                     {
-                      term.content.map((para) => { return (
-                        <p className='mt-2 mb-5 text-medium'>{para}</p>
+                      term.paragraphs.map((para) => { return (
+                        <>
+                        <p className='mt-2 mb-5 text-medium'>{para}
+                          <a className='text-blue-700' href={`mail.to:${term.emailLink}`}>{term.emailLink && term.emailLink}</a>
+                        </p>
+                        </>
                       )})
                     }
+                    <ul>
+                      { term.lists &&
+                        term.lists.map((listItem,index) => { return (
+                          <li>{listItem} <a href={`https://${term.links[index]}`} className="text-blue-700">{term.links[index]}</a></li>
+                        )})
+                      }
+                    </ul>
                   </div>
                 )
               })
