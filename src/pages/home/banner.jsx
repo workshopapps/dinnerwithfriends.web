f2ab8b5da521912d78040b2b4705c944678577c7
Donice <doniceubaru1@gmail.com>
2022-11-20 21:36:58 +0100

    done

---
+++ src/pages/home/banner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  return (
+    <section class="bg-blue-100">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <h1 class="hidden mb-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:block md:text-5xl lg:text-6xl">
+          <span className="block mb-2 lg:mb-4">Organize your next social</span>
+          <span className="mb-2 lg:mb-4">gathering, on </span>
+          <span className="text-blue-600">Catch</span>
+          <span>Up</span>
+        </h1>
+        <h1 class="mb-8 text-4xl font-extrabold tracking-tight leading-8text-gray-900 md:hidden md:text-5xl lg:text-6xl">
+          <span className="block mb-2 ">Organize your next</span>
+          <span className="mb-2"> social gathering, on </span>
+          <span className="mb-2 text-blue-600">Catch</span>
+          <span>Up</span>
+        </h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+          Look no further, Get started today.
+        </p>
+        <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+          <Link
+            to="#"
+            class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
+          >
+            Get Started
+            <svg
+              class="ml-2 -mr-1 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
---
+++ src/pages/home/heroDashboard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import HomeDashboard from "../../assets/img/HomeDashboard.png"
+
+const HeroDashboard = () => {
+  return (
+    <section className="bg-[url('assets/img/VectorsHome.png')] bg-no-repeat bg-bottom text-black">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <Link
+          to="#"
+          className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full"
+          role="alert"
+        >
+          <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
+            New
+          </span>
+          <span className="text-sm font-medium">
+            See what's new!
+          </span>
+          <svg
+            className="ml-2 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </Link>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+          Perform all actions in your Dashboard.
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">
+          We Built a dashboard, that is just for you, where various actions can
+          be performed seamlessly.
+        </p>
+
+        <div className="mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+          <img src={HomeDashboard} alt="dashboard" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroDashboard;
