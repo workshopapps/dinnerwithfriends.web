90d957169dfbe63738368f622b1ffe2a2ea53c6b
vickymarz <okorojiebube2@gmail.com>
2022-11-20 15:23:27 +0100

    fro-24:Add Faq route

--- src/pages/FAQ/Faq.jsx
+++ src/pages/FAQ/Faq.jsx
@@ -3,12 +3,13 @@ import Navbar from "../../components/Navbar";
 import Accordions from "./Accordions";
 import people from '../../assets/img/faq-models.png'
 import Button from "../../components/Button";
+import Footer from "../../components/Footer";
 
 const Faq = () => {
   return (
     <>
       <Navbar />
-        <section className="relative top-[96px] lg:top-[132px]">
+        <section className="mt-[120px] lg:mt-[125px]">
           <div className="flex flex-col justify-center items-center gap-y-3 lg:gap-y-6 mb-16 lg:mb-40">
             <h2 className="text-[#151517] font-bold text-xl lg:text-5xl">Frequently Asked Questions</h2>
             <p className="text-[#9F9F9F] text-xs lg:text-2xl">Everything to know about Catch - Up</p>
@@ -16,7 +17,7 @@ const Faq = () => {
           <div className="w-[90%] lg:w-6/12 m-auto">
             <Accordions />
           </div>
-          <div className="mt-[64px] lg:mt-[137px] w-[90%] lg:w-[80%] mx-auto bg-[#E7F0FF] rounded flex flex-col justify-center items-center gap-y-[10px] lg:gap-y-6 py-5">
+          <div className="mt-[64px] mb-[60px] lg:mb-[108px] lg:mt-[137px] w-[90%] lg:w-[80%] mx-auto bg-[#E7F0FF] rounded flex flex-col justify-center items-center gap-y-[10px] lg:gap-y-6 py-5">
             <div className="w-[46px] lg:w-[82px] h-15px lg:h-[30px]">
                 <img src={people} alt="" />
             </div>
@@ -27,6 +28,7 @@ const Faq = () => {
             </Button>
           </div>
         </section>
+        <Footer />
     </>
   )
 }
