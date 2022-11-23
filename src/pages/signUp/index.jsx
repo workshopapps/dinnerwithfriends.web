5eed197d38b2bb9962a4a48c0e5a1f3d4ea75e96
Victor Ebube Okoroji <okorojiebube2@gmail.com>
2022-11-23 12:19:46 +0100

    Update index.jsx

--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -13,7 +13,7 @@ const SignUp = () => {
   !errors.email ? console.log(' no email error') : console.log(' email error')
   const onSubmit = (data) => {
     console.log(data)
-    !errors.email && navigate('/create_event')
+    !errors.email && navigate('/dashboard/upcoming_events')
   }
   /* eslint-disable-next-line */
   const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
