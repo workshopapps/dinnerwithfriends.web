320d17873b34a49a52ebdec22d8f9f7ace6d7ad0
vickymarz <okorojiebube2@gmail.com>
2022-11-23 10:10:43 +0100

    Fix merge comflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import "./index.css";
 import CreateEvent from "./pages/dashboard/createEvent/CreateEvent";
 import Home from "./pages/home";
-import Invitee from "./pages/invitee";
-import ClosedEvent from "./pages/closedEvent";
+import Invitee from "./pages/dashboard/Invitee";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import ViewEvent from "./pages/dashboard/ViewEvent";
-import ContactUs from "./pages/contactUs";
-import ProfilePage from "./pages/settingsPage/profilePage";
-import ForgetPassword from "./pages/ForgetPassword/index";
-import Policy from "./pages/policy";
-import Error404 from "./pages/error404";
-import HowItWorks from "./pages/howItWorks/HowItWorks";
-import Terms from "./pages/t&c";
-import TermsOfUse from "./pages/termsOfUse";
-import Faq from "./pages/FAQ/Faq";
-import Settings from "./pages/settingsPage/profilePage"
-import EventInvite from "./pages/invitee/EventInvite";
-import EventInviteResponse from "./pages/invitee/EventInviteResponse";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
 import EventSummary from "./pages/dashboard/EventSummary";
 import UpcomingEvent from "./pages/dashboard/UpcomingEvent";
 
@@ -28,26 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create_event" element={<CreateEvent />} />
-        <Route path='/closed_event' element={<ClosedEvent />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/error404" element={<Error404 />} />
-        <Route path="/invitee" element={<Invitee />} />
+        {/* <Route path="/invitee" element={<Invitee />} /> */}
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/dashboard/view_event" element={<ViewEvent />} />
         <Route path="/view_event" element={<ViewEvent />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/policy" element={<Policy />} />
-        <Route path="/howItWorks" element={<HowItWorks />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/terms-of-use" element={<TermsOfUse />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/event_invite" element={<EventInvite />} />
-        <Route path="/event_summary" element={<EventSummary />} />
-        <Route path="/event_invite/event_invite_response" element={<EventInviteResponse />} />
-        <Route path="/dashboard/home" element={<UpcomingEvent />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/dashboard/event_summary" element={<EventSummary />} />
+        <Route path="/dashboard/upcoming_events" element={<UpcomingEvent />} />
       </Routes>
     </div>
   );
--- src/pages/dashboard/UpcomingEvent.jsx
+++ src/pages/dashboard/UpcomingEvent.jsx
@@ -1,162 +1,55 @@
-import React, { useState} from 'react'
-import Button from '../../components/Button'
-import calender from '../../assets/img/g-calendar.png'
-import MediaQuery from 'react-responsive'
-import CreateEventNavbar from "../../components/CreateEvent/CreateEventNavbar";
-import Event from '../../components/Event'
-import add from '../../assets/img/add.png'
-import Footer from '../../components/Footer'
-import { Link } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+import CreateEvent from "./pages/dashboard/createEvent/CreateEvent";
+import Home from "./pages/home";
+import Invitee from "./pages/invitee";
+import ClosedEvent from "./pages/closedEvent";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signUp";
+import ViewEvent from "./pages/dashboard/ViewEvent";
+import ContactUs from "./pages/contactUs";
+import ProfilePage from "./pages/settingsPage/profilePage";
+import ForgetPassword from "./pages/ForgetPassword/index";
+import Policy from "./pages/policy";
+import Error404 from "./pages/error404";
+import HowItWorks from "./pages/howItWorks/HowItWorks";
+import Terms from "./pages/t&c";
+import TermsOfUse from "./pages/termsOfUse";
+import Faq from "./pages/FAQ/Faq";
+import Settings from "./pages/settingsPage/profilePage"
+import EventInvite from "./pages/invitee/EventInvite";
+import EventInviteResponse from "./pages/invitee/EventInviteResponse";
+import EventSummary from "./pages/dashboard/EventSummary";
+import UpcomingEvent from "./pages/dashboard/UpcomingEvent";
 
-const UpcomingEvent = () => {
-  const [status, setStatus] = useState('Upcoming');
-  const event = [
-    {
-      id: 1,
-      title: 'Catch Up with Football (UEFA Championship)',
-      description: 'Guys! It’s been long we have gathered, let’s try to make time for champions league next week tuesday. If you know your GOAT no qualify, no bring yourself here.',
-      status: 'Upcoming',
-      date: '21st November',
-      invitee: '2'
-    },
-    {
-      id: 2,
-      title: 'Catch Up with Football (UEFA Championship)',
-      description: 'Guys! It’s been long we have gathered, let’s try to make time for champions league next week tuesday. If you know your GOAT no qualify, no bring yourself here.',
-      status: 'Upcoming',
-      date: '21st November',
-      invitee: '3',
-    },
-    {
-      id: 3,
-      title: 'Catch Up with Football (UEFA Championship)',
-      description: 'Guys! It’s been long we have gathered, let’s try to make time for champions league next week tuesday. If you know your GOAT no qualify, no bring yourself here.',
-      status: 'Rsvp',
-      date: '21st November',
-      invitee: '5',
-    },
-    {
-      id: 4,
-      title: 'Catch Up with Football (UEFA Championship)',
-      description: 'Guys! It’s been long we have gathered, let’s try to make time for champions league next week tuesday. If you know your GOAT no qualify, no bring yourself here.',
-      status: 'Rsvp',
-      date: '21st November',
-      invitee: '7',
-    },
-    {
-      id: 5,
-      title: 'Catch Up with Football (UEFA Championship)',
-      description: 'Guys! It’s been long we have gathered, let’s try to make time for champions league next week tuesday. If you know your GOAT no qualify, no bring yourself here.',
-      status: 'Pending',
-      date: '21st November',
-      invitee: '3',
-    },
-    {
-      id: 6,
-      title: 'Catch Up with Football (UEFA Championship)',
-      description: 'Guys! It’s been long we have gathered, let’s try to make time for champions league next week tuesday. If you know your GOAT no qualify, no bring yourself here.',
-      status: 'Pending',
-      date: '26th November',
-      invitee: '5',
-    },
-  ]
-
-    const filteredEvents = event.filter((event) => event.status === status )
-
-    const events = filteredEvents.map(({id, title, description, date, invitee}) => (
-      <Event status={status} key={id} title={title} description={description} date={date} invitee={invitee} filteredEvents={filteredEvents}/>
-    ))
-
-    return (
-        <>
-        <CreateEventNavbar />
-        <section className='px-[22px] lg:px-20 pt-11 lg:pt-16 mb-16'>
-          <div className='flex flex-col lg:flex-row justify-center lg:justify-between gap-x-4 mb-4 lg:mb-8'>
-            <div className='flex flex-col justify-center items-center lg:justify-start lg:items-start gap-y-4'>
-               <h2 className='text-[#151517] text-2xl font-bold lg:text-[40px]'>Welcome</h2>
-               <p className='text-[#898989] text-center font-bold lg:font-normal text-sm lg:text-lg'>Create events, send invites and keep up with your friends</p>
-            </div>
-            <Button type='button' className='hidden lg:flex justify-between items-center gap-x-3.5 rounded-lg border border-solid border-[#344054] px-4 outline-0'>
-              <div className='w-[28px] h-[28px]'>
-                <img src={calender} alt="google-calender" />
-              </div>
-              <p className='text-[#344054] text-lg'>Sync with Google Calendar</p>
-            </Button>
-          </div>
-          <div className='flex flex-col p-8 gap-y-8 mt-9 justify-center items-center border border-solid border-[#CDCDCD] bg-[#FAFAFA] lg:bg-white'>
-            <Button type='button' className=' text-white bg-[#1070FF] rounded py-3 px-4 lg:px-8 outline-0'>
-              <Link to="/create_event" className='font-bold text-sm lg:text-xl flex justify-between items-center gap-x-3.5 text-white'>
-                <span>
-                  <img src={add} alt="add" />
-                </span>
-                <span>Create Event</span>
-              </Link>
-            </Button>
-            <div className='flex flex-col justify-center items-center gap-y-2'>
-              <span className='text-[#151517] font-bold lg:font-medium text-xl'>New Catch Up Event</span>
-              <span className='text-[#898989] font-bold lg:font-normal text-lg text-center'>Create event and let people chip in</span>
-            </div>
-          </div>
-          <div className='lg:border border-solid border-[#CDCDCD] px-[46px] py-9'>
-             <h3 className='text-[#151517] text-center lg:text-left font-bold lg:font-medium text-2xl lg:mb-[40px]'>All Events</h3>
-             <MediaQuery minWidth={1024}>
-               <ul className='flex justify-start items-center gap-x-8'>
-                <li>
-                  <Button className={`${status === 'Upcoming' ? 'pb-3 border-[#1070FF] border-b-4 '  : ''}'pb-3 outline-0 border-0 text-[#717172] bg-inherit text-lg'`} onClick={() => setStatus('Upcoming')}>
-                    Upcoming Event
-                  </Button>
-                </li>
-                <li>
-                  <Button className={`${status === 'Rsvp' ? 'pb-3 border-[#1070FF] border-b-4 '  : ''}' pb-3 outline-0 border-0 text-[#717172] bg-inherit text-lg'`} onClick={() => setStatus('Rsvp')}>
-                   Rsvp Event
-                  </Button>
-                </li>
-                <li>
-                  <Button className={`${status === 'Pending' ? 'pb-3 border-[#1070FF] border-b-4 '  : ''}' pb-3 outline-0  text-[#717172] bg-inherit text-lg'`} onClick={() => setStatus('Pending')}>
-                    Pending Event
-                  </Button>
-                </li>
-               </ul>
-              </MediaQuery>
-          </div>
-          <div className='flex flex-col justify-center items-center gap-y-8 lg:border border-solid border-[#CDCDCD] lg:pt-12 lg:pb-[200px] px-[20px] 8lg:px-12'>
-            <MediaQuery minWidth={1024}>
-              {status === 'Upcoming' && (
-                <div className='lg:grid lg:grid-cols-2 grid-flow-row content-start gap-4 px-8'>
-                  {events}
-                </div>
-              )}
-              {status === 'Rsvp' && (
-                <div className='lg:grid lg:grid-cols-2 grid-flow-row content-start gap-4 px-8'>
-                {events}
-              </div>
-              )}
-              {status === 'Pending' && (
-              <div className='lg:grid lg:grid-cols-2 grid-flow-row content-start gap-4 px-8'>
-                {events}
-              </div>
-              )}
-            </MediaQuery>
-            <MediaQuery maxWidth={1024}>
-            <div>
-              <p className='text-[#424245] font-medium mb-4 text-center'>Upcoming Events</p>
-             { events}
-            </div>
-            <div>
-              <p className='text-[#424245] font-medium mb-4 text-center'>Rsvp Events</p>
-            { events}
-            </div>
-            <div>
-              <p className='text-[#424245] font-medium mb-4 text-center'>Pending Events</p>
-              {events}
-            </div>
-            </MediaQuery>
-          </div>
-        </section>
-        <Footer />
-        </>
-      )
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create_event" element={<CreateEvent />} />
+        <Route path='/closed_event' element={<ClosedEvent />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/error404" element={<Error404 />} />
+        <Route path="/invitee" element={<Invitee />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/view_event" element={<ViewEvent />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/howItWorks" element={<HowItWorks />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/terms-of-use" element={<TermsOfUse />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/event_invite" element={<EventInvite />} />
+        <Route path="/event_summary" element={<EventSummary />} />
+        <Route path="/event_invite/event_invite_response" element={<EventInviteResponse />} />
+        <Route path="/dashboard/home" element={<UpcomingEvent />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default UpcomingEvent
-
+export default App;
