20a1fc529ac0da84943ff2a39a490da583b78e54
Pappyjay23 <pjinadu02@gmail.com>
2022-11-30 00:13:55 +0100

    fro-70: I made the catchup logo consistent across all pages

--- src/components/CreateEvent/CreateEventForm.jsx
+++ src/components/CreateEvent/CreateEventForm.jsx
@@ -47,7 +47,6 @@ const CreateEventForm = () => {
 		if (Object.keys(errors).length === 0) {
 			submitForm(formValues);
 		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [errors, navigate]);
 
 	return (
