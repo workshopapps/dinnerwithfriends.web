e3a2bc2b86cac191f368cc7ba47b5dfac5c2e6b3
vickymarz <okorojiebube2@gmail.com>
2022-11-30 16:30:10 +0100

    Add calender api endpoint

--- src/services/rootEndPoints.js
+++ src/services/rootEndPoints.js
@@ -5,4 +5,5 @@ export const PASSWORD_RECOVERY_URL ='auth/recover/generate'
 export const RESET_PASSWORD_URL = 'auth/recover/confirm'
 export const CREATE_EVENT = 'event'
 export const GET_EVENTS = 'event'
+export const GOOGLE_CALENDER = 'calender'
 
