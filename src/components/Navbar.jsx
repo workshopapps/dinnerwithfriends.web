d0c706ab21492684fc00a1af19aa26a7c9bae7a2
vickymarz <okorojiebube2@gmail.com>
2022-12-01 23:49:17 +0100

    Fix internal errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import React from "react";
 import { Link } from "react-router-dom";
-import { HiChevronDown } from "react-icons/hi";
-import Logo from "./Logo";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
   return (
     <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-white">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <Logo />
+        <Link to="/" className="flex items-center">
+          <span className="self-center text-[#0056D6] text-3xl font-semibold whitespace-nowrap ">
+            Catch
+          </span>
+          <span className="self-center text-black text-3xl font-semibold whitespace-nowrap">
+            Up
+          </span>
+        </Link>
         <div className="flex md:order-2">
-          <Link to="/sign_in" className="mr-5 mt-2 text-blue-700 font-bold">
-            Log In
-          </Link>
-
           <Link to="/sign_up">
             <button
               type="button"
@@ -23,16 +22,38 @@ const Navbar = () => {
               Sign up
             </button>
           </Link>
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
-        <div style={nav}
-          className="transition linear duration-300 items-center justify-between  w-full md:flex md:w-auto md:order-1"
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul  className="bg-green-500 w-full flex flex-col p-4 mt-0 md:mt-4 border border-white rounded-lg bg-white md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
+          <ul className="flex flex-col p-4 mt-4 border border-white rounded-lg bg-white md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
             <li>
               <Link
                 to="/howItWorks"
-                className="block py-3 md:py-2 pl-3 pr-4 text-black hover:text-white hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 rounded md:bg-transparent md:p-0 dark:text-white"
+                className="block py-2 pl-3 pr-4 text-white bg-[#0056D6] rounded md:bg-transparent md:text-[#0056D6] md:p-0 dark:text-white"
                 aria-current="page"
               >
                 How it works
@@ -41,7 +62,7 @@ const Navbar = () => {
             <li>
               <Link
                 to="/error404"
-                className="block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                className="block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#0056D6] md:p-0 "
               >
                 Blog
               </Link>
@@ -49,7 +70,7 @@ const Navbar = () => {
             <li>
               <Link
                 to="/error404"
-                className="flex  py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                className="flex py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#0056D6] md:p-0 "
               >
                 Company
                 <svg
@@ -71,77 +92,12 @@ const Navbar = () => {
                 fro-17-features-page
                 to="/features"
                 dev
-                className="block py-3 md:py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                className="block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#0056D6] md:p-0 "
               >
                 Features
               </Link>
             </li>
-            <li>
-              <Link
-                
-                to="/aboutus"
-                dev
-                className="block py-3 md:py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
-              >
-                About Us
-              </Link>
-            </li>
-            <li className="md:hidden">
-              <Link
-                
-                to="/sign_up"
-                dev
-                className=" mx-auto block mt-4 py-2 pl-3 pr-4 text-white text-center bg-[#1070FF] h-[44px] w-[225px] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
-              >
-                Sign Up
-              </Link>
-            </li>
           </ul>
-          
-        </div>
-        <div className="md:hidden">
-          <div
-            className="cursor-pointer pr-4 z-100 text-gray-800 "
-            onClick={() => setNav(!nav)}
-          >
-            {nav ? <FaTimes size={20} /> : <FaBars size={30} />}
-          </div>
-          {nav && (
-            <ul className="flex flex-col p-4 items-center justify-center  text-white md:hidden bg-blue-700 absolute top-10 right-0 w-60 h-150  ">
-              <li>
-                <Link to="/howItWorks">How it works</Link>
-              </li>
-              <li>
-                <Link to="/error404">Blog</Link>
-              </li>
-              <li>
-                <Link
-                  to="/error404"
-                  className="flex flex-row items-center justify-center space-x-1"
-                >
-                  <p>Company</p>
-                  <HiChevronDown />
-                  {/*<svg
-        className="w-5 h-5 ml-1"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clipRule="evenodd"
-        ></path>
-          </svg>*/}
-                </Link>
-              </li>
-              <li>
-                <Link fro-17-features-page to="/features">
-                  Features
-                </Link>
-              </li>
-            </ul>
-          )}
         </div>
       </div>
     </nav>
