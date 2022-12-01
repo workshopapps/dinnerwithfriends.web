6f607b3956b5636c788804accab824404a056ea1
Mayana <oluwapelumisotoyinbo@gmail.com>
2022-12-01 22:21:28 +0100

    fix

--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -64,7 +64,15 @@ const SignIn = () => {
                 <span className="font-bold text-3xl lg:text-5xl ml-1">Up</span>
               </Link>
               </div>
+              <div className='px-1 rounded-[20px] w-29 tablet:w-35 tablet:h-10 h-[34px]  bg-blue-100 flex justify-around items-center'>
 
+                <select className='language-select bg-blue-100 w-full font-semibold focus:outline-none text-xs tablet:text-sm' name="language" id="language">
+                  <option value="uk">English (UK)</option>
+                  <option value="us">English (US)</option>
+                </select>
+
+
+              </div>
             </div>
             <div className="px-2 lg:px-14 mt-10 lg:mt-14">
               <h3 className="font-medium text-xl lg:text-4xl font-bold text-gray-600 ">
@@ -98,7 +106,7 @@ const SignIn = () => {
 
                 <label className="pb-0">Password</label>
                 <input
-                  className="relative p-2 rounded-xl border"
+                  className="p-2 rounded-xl border"
                   type="password"
                   name="password"
                   id="password"
