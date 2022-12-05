740d66f6c2746953739608e78cb84a5cd9490837
Mayana <oluwapelumisotoyinbo@gmail.com>
2022-12-05 07:29:58 +0100

    fix the navbar dropdown issue

--- src/components/CreateEvent/CreateEventNavbar.jsx
+++ src/components/CreateEvent/CreateEventNavbar.jsx
@@ -11,7 +11,23 @@ import { Link } from "react-router-dom";
 
 const CreateEventNavbar = () => {
 	const [open, setOpen] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
+  const toggleOpen = () => {
+    setOpen(!open);
+    setShowNav(false);
+  }
+
+  const handleOpen = () => {
+    setShowNav(true);
+    setOpen(false);
+  }
+
+  const handleClose = () => {
+    setShowNav(false);
+    setOpen(false);
+  }
+ 
 	return (
 		<div className="border border-[#E3E3E3]">
       <div className={menuStyles.theCreateEventContainer}>
@@ -19,53 +35,94 @@ const CreateEventNavbar = () => {
         <div className="items-center cursor-pointer hidden md:flex">
           <img src={bell} alt="" className="w-[15px] h-[15px] mr-2" />
           <div 
-            onClick={() => setOpen(!open)}
+            onClick={toggleOpen}
             className="w-[35px] h-[35px] border border-[#0056D6] text-[#717172] rounded-full flex justify-center items-center object-cover text-xs">
             <span>M</span>
           </div>
         </div>
-        <div className=" md:hidden flex items-center">
-          <button className={open && menuStyles.close}>
-            <HiOutlineMenu onClick={() => setOpen(true)} />
+        <div className="md:hidden flex items-center">
+          <button className={showNav && menuStyles.close}>
+            <HiOutlineMenu onClick={handleOpen} />
           </button>
-          <button className={!open && menuStyles.open}>
-            <FaTimes onClick={() => setOpen(false)} />
+          <button className={!showNav && menuStyles.open}>
+            <FaTimes onClick={handleClose} />
           </button>
         </div>
       </div>
 
-      <div class="text-left">
+      <div class="text-left hidden md:block items-center">
         {open && 
-          <div class="absolute right-6 z-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+          <div class="absolute items-center right-6 z-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class={`${menuStyles.goTo} py-3 px-2`} role="none">
               <ul>
-                <li>
+                <li to="/profile">
                   <BsPerson />
-                  <Link to="/dashboard/upcoming_events" className="block px-2 py-2 text-sm">
+                  <Link to="/profile" className="block px-2 py-2 text-sm w-full">
                     Profile
                   </Link>
                 </li>
                 <li>
                   <BiPurchaseTag />
-                  <Link to="/#" className="block px-2 py-2 text-sm">
+                  <Link to="/faq" className="block px-2 py-2 text-sm w-full">
                     Faq
                   </Link>
                 </li>
                 <li>
                   <BiLock />
-                  <Link to="/policy" className="block px-2 py-2 text-sm">
+                  <Link to="/policy" className="block px-2 py-2 text-sm w-full">
                     Privacy
                   </Link>
                 </li>
                 <li>
                   <FiSettings />
-                  <Link to="/profile" className="block px-2 py-2 text-sm">
+                  <Link to="/profile" className="block px-2 py-2 text-sm w-full">
                     Account Setting
                   </Link>
                 </li>
                 <li>
                   <FiLogIn />
-                  <Link to="/error404" className="block px-2 py-2 text-sm">
+                  <Link to="/error404" className="block px-2 py-2 text-sm w-full">
+                    Log Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        }
+      </div>
+
+      <div class="text-left md:hidden block">
+        {showNav && 
+          <div class="absolute right-6 z-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+            <div class={`${menuStyles.goTo} py-3 px-2`} role="none">
+              <ul>
+                <li to="/profile">
+                  <BsPerson />
+                  <Link to="/profile" className="block px-2 py-2 text-sm w-full">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <BiPurchaseTag />
+                  <Link to="/faq" className="block px-2 py-2 text-sm w-full">
+                    Faq
+                  </Link>
+                </li>
+                <li>
+                  <BiLock />
+                  <Link to="/policy" className="block px-2 py-2 text-sm w-full">
+                    Privacy
+                  </Link>
+                </li>
+                <li>
+                  <FiSettings />
+                  <Link to="/profile" className="block px-2 py-2 text-sm w-full">
+                    Account Setting
+                  </Link>
+                </li>
+                <li>
+                  <FiLogIn />
+                  <Link to="/error404" className="block px-2 py-2 text-sm w-full">
                     Log Out
                   </Link>
                 </li>
