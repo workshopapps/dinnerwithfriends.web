8607b9cd1f1b7884682b0b14041df3f6508384a7
Precious Adedibu <adedibuprecious@gmail.com>
2022-12-11 13:12:13 +0100

    debugging

--- src/pages/verify/index.jsx
+++ src/pages/verify/index.jsx
@@ -6,6 +6,7 @@ const Verify = () => {
   const [cookies] = useCookies();
   const navigate = useNavigate();
   useEffect(() => {
+    console.log(cookies);
     localStorage.setItem("jwt-token", cookies.accessToken);
     navigate("/dashboard/upcoming_events");
   }, []);
