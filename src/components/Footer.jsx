1dc3409b7b36cd268b9bfa014ab66aaf8fbd3473
Oluyedi Oluwasegun Stephen <oluyedis@gmail.com>
2023-01-12 08:25:50 +0100

    FIX(fec-145-external-internal-page-footer)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,14 +17,14 @@ const Footer = () => {
     { title: "Terms & Conditions", to: "/terms-of-use" },
   ];
   return (
-    <footer className="bg-[#0056D6] overflow-hidden">
+    <footer className="bg-[#1070FF] overflow-hidden">
       <div className="block md:flex justify-between ">
         <div>
-          <Logo footer className="mx-auto lg:mx-20 py-8 px-6" />
+          <Logo footer className="lg:mx-12 py-8 px-6" />
         </div>
 
         <div>
-          <div className="mx-auto lg:mx-24 grid grid-cols-2 gap-8 md:gap-12 py-8 px-6 md:grid-cols-3">
+          <div className="mx-auto lg:mx-12 grid grid-cols-2 gap-8 md:gap-12 py-8 px-6 md:grid-cols-3">
             <div>
               <h2 className="mb-3 lg:mb-6 text-sm font-semibold text-gray-100 uppercase">
                 Company
@@ -70,11 +70,11 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="mx-auto lg:mx-24 py-6 px-4 text-center gap-4 flex flex-col-reverse  md:flex md:flex-row md:items-center md:justify-between">
+      <div className="mx-auto lg:mx-12 py-6 px-4 text-center gap-4 flex flex-col-reverse  md:flex md:flex-row md:items-center md:justify-between">
         <span className="text-md text-gray-100 sm:text-center">
           © 2022 <Link href="/">Team CatchUp™</Link>
         </span>
-        <div className="flex mt-1 text-md text-white text-center space-x-12 justify-center md:mt-0">
+        <div className="flex mt-1 text-md text-white text-center space-x-8 justify-center md:mt-0">
           <a href="https://www.facebook.com/profile.php?id=100088950369086&mibextid=LQQJ4d">
             <BsFacebook />
           </a>
@@ -84,7 +84,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/company/justcatchup/">
             <BsLinkedin />
           </a>
-          <a href="/">
+          <a href="https://youtube.com/@justcatchup">
             <BsYoutube />
           </a>
         </div>
