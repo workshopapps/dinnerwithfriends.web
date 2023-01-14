49a9dff113f6a9e12a9b219b7a37572370a93e03
Treasure Ajefu <treasureajefu@gmail.com>
2023-01-14 18:19:46 +0100

    ditched old code

--- src/pages/howItWorks/HowItWorks.jsx
+++ src/pages/howItWorks/HowItWorks.jsx
@@ -1,15 +1,12 @@
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
-import FirstComponent from "./FirstComponent";
-import Howitwork2 from "./Howitwork2";
 
 export default function HowItWorks() {
   return (
     <div>
       <Navbar />
       <section>
-        <FirstComponent />
-        <Howitwork2/>
+        
       </section>
       <Footer />
     </div>
