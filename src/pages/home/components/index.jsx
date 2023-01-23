81f6529ecf662685d4e5ac17af58bc0c2761f4d0
vickymarz <okorojiebube2@gmail.com>
2023-01-23 01:56:50 +0100

    Add banner landing page section

--- src/pages/home/components/index.jsx
+++ src/pages/home/components/index.jsx
@@ -1,2 +1,3 @@
 export * from './hero'
 export * from './schedule'
+export * from './banner'
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import {Footer} from "../../components";
-import {Hero, Schedule} from "./components";
+import {Hero, Schedule, Banner} from "./components";
 import Testimonials from "./testimonials";
-import Banner from "./banner";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="">
-        <Hero />
-        <Schedule />
-        <Testimonials />
-      </div>
+      <Hero />
+      <Schedule />
+      <Testimonials />
       <Banner />
       <Footer />
     </>
